701144d327ed467782941a8ae57e9dc42ab02bc3
Evgeny Evsyukov <henzerboss@gmail.com>
2025-07-30 17:01:47 +0200

    feat: add custom server to load env variables

---
+++ server.js
@@ -0,0 +1,26 @@
+// file: server.js
+
+// Эта строка - самое главное. Она загружает все переменные из вашего .env файла.
+require('dotenv').config({ path: './.env' });
+
+// Этот код просто запускает ваш обычный Next.js сервер
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+// Берем порт из package.json или по умолчанию
+const port = parseInt(process.env.npm_config_port, 10) || 10000;
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
