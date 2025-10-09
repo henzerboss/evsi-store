5fb8587b594cba2d55d343d20907291478ed719e
Evgeny Evsyukov <henzerboss@gmail.com>
2025-10-09 11:25:11 +0200

    redirect of octosubs

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/en/app/subscrab/privacy',
+        destination: '/en/app/octosubs/privacy',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/en/app/subscrab/privacy',
+        destination: '/en/app/octosubs/privacy',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
